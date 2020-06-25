922eec660ee6e0642d5d8ca3c8a3fecff69d6d43
kiddac <kiddac2015@gmail.com>
2020-06-25 01:10:36 +0100

    Syntax clean up

--- XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/main.py
+++ XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/main.py
@@ -18,7 +18,7 @@ try:
 	from urlparse import urlparse, parse_qs
 except:
 	from urllib.parse import urlparse, parse_qs
-	
+
 from xStaticText import StaticText
 
 import json
@@ -34,7 +34,6 @@ except:
 from multiprocessing.pool import ThreadPool
 
 
-
 class XStreamity_Main(Screen):
 
 	def __init__(self, session):
@@ -47,8 +46,6 @@ class XStreamity_Main(Screen):
 
 		self.list = []
 		self['menu'] = List(self.list, enableWrapAround=True)
-		
-
 		self.setup_title = (_('Select Server'))
 
 		self['key_red'] = StaticText(_('Back'))
@@ -63,30 +60,26 @@ class XStreamity_Main(Screen):
 		self.drawList = []
 		self["playlists"] = List(self.drawList)
 		self['playlists'].onSelectionChanged.append(self.getCurrentEntry)
-
 		self["splash"] = Pixmap()
 		self["splash"].hide()
-		
 		self["scroll_up"] = Pixmap()
 		self["scroll_down"] = Pixmap()
-
 		self["scroll_up"].hide()
 		self["scroll_down"].hide()
 
 		glob.configchanged = True
 
 		self['actions'] = ActionMap(['XStreamityActions'], {
-		'red': self.quit,
-		'green': self.addServer,
-		'yellow': self.editServer,
-		'blue': self.deleteServer,
-		'cancel': self.quit,
-		'info': self.openUserInfo,
-		'ok' :  self.getStreamTypes,
-		'menu' : self.settings
-
+			'red': self.quit,
+			'green': self.addServer,
+			'yellow': self.editServer,
+			'blue': self.deleteServer,
+			'cancel': self.quit,
+			'info': self.openUserInfo,
+			'ok': self.getStreamTypes,
+			'menu': self.settings
 		}, -2)
-			
+
 		self.onFirstExecBegin.append(self.start)
 		self.onLayoutFinish.append(self.__layoutFinished)
 
@@ -102,26 +95,24 @@ class XStreamity_Main(Screen):
 			system("echo 3 > /proc/sys/vm/drop_caches")
 		except:
 			pass
-		
+
 
 	def start(self):
-		#get version number
+		# get version number
 		self.clear_caches()
-		
+
 		self['version'].setText(VERSION)
 
 		# check if playlists.json file exists in specified location
 		self.playlists_all = []
-		
 
 		if os.path.isfile(json_file):
 			with open(json_file, "rb") as f:
 				try:
-					#self.playlists_all = json.load(f, object_pairs_hook=OrderedDict)
+					# self.playlists_all = json.load(f, object_pairs_hook=OrderedDict)
 					self.playlists_all = json.load(f)
 				except:
 					os.remove(json_file)
-		
 
 		if os.path.isfile(playlist_path) and os.stat(playlist_path).st_size > 0:
 			self.checkPlaylistUserFile()
@@ -129,13 +120,12 @@ class XStreamity_Main(Screen):
 
 	def checkPlaylistUserFile(self):
 		self["splash"].show()
-		
+
 		with open(playlist_path, 'r+') as f:
 			lines = f.readlines()
 			f.seek(0)
 			f.writelines((line.strip(' ') for line in lines if line.strip()))
 			f.truncate()
-			
 			f.seek(0)
 			for line in lines:
 				if not line.startswith('http://') and not line.startswith('https://') and not line.startswith('#'):
@@ -148,15 +138,12 @@ class XStreamity_Main(Screen):
 					line = ""
 				f.write(line)
 			f.truncate()
-			
-				
 		self.getPlaylistUserFile()
 
 
 	def getPlaylistUserFile(self):
 		with open(playlist_path, "rb") as f:
 			lines = f.readlines()
-			#f.seek(0)
 			self.index = 0
 
 			for line in lines:
@@ -181,118 +168,111 @@ class XStreamity_Main(Screen):
 				self.epgquickshift = 0
 
 				parsed_uri = urlparse(line)
-				
+
 				self.protocol = parsed_uri.scheme + "://"
-				
+
 				if not (self.protocol == "http://" or self.protocol == "https://"):
 					continue
-					
+
 				self.domain = parsed_uri.hostname
-				
+
 				if parsed_uri.port:
 					self.port = parsed_uri.port
+				self.host = "%s%s:%s" % (self.protocol, self.domain, self.port)
 
-				self.host =  "%s%s:%s" % (self.protocol, self.domain, self.port)
-				
 				query = parse_qs(parsed_uri.query, keep_blank_values=True)
-				
+
 				if "username" in query:
 					self.username = query['username'][0]
 				else:
 					continue
-					
+
 				if "password" in query:
 					self.password = query['password'][0]
 				else:
 					continue
-				
+
 				if "type" in query:
 					self.type = query['type'][0]
-					
-					
+
 				if "output" in query:
 					self.output = query['output'][0]
-					
-				
+
 				self.player_api = "%s/player_api.php?username=%s&password=%s" % (self.host, self.username, self.password)
 				self.enigma2_api = "%s/enigma2.php?username=%s&password=%s" % (self.host, self.username, self.password)
 				self.xmltv_api = "%s/xmltv.php?username=%s&password=%s" % (self.host, self.username, self.password)
 				self.full_url = "%s/get.php?username=%s&password=%s&type=%s&output=%s" % (self.host, self.username, self.password, self.type, self.output)
-				
+
 				playlist_exists = False
-		
+
 				if self.playlists_all:
 					for playlists in self.playlists_all:
-		
-						#extra check in case playlists.txt details have been amended
+
+						# extra check in case playlists.txt details have been amended
 						if "domain" in playlists["playlist_info"] and "username" in playlists["playlist_info"] and "password" in playlists["playlist_info"]:
 							if playlists["playlist_info"]["domain"] == self.domain and playlists["playlist_info"]["username"] == self.username and playlists["playlist_info"]["password"] == self.password:
 								playlist_exists = True
 								playlists["playlist_info"]["type"] = self.type
 								playlists["playlist_info"]["output"] = self.output
-								playlists["playlist_info"]["full_url"] = self.full_url #get.php
+								playlists["playlist_info"]["full_url"] = self.full_url  # get.php
 
 								if "epgtype" not in playlists["player_info"]:
 									playlists["player_info"]["epgtype"] = self.epgtype
-									
+
 								if "epgquickshift" not in playlists["player_info"]:
 									playlists["player_info"]["epgquickshift"] = self.epgquickshift
-									
-				if playlist_exists == False:
+
+				if playlist_exists is False:
 					self.playlists_all.append({
-					"playlist_info": dict([
-						("index", self.index),
-						("name", self.domain),
-						("protocol",self.protocol),
-						("domain", self.domain),
-						("port", self.port),
-						("username", self.username),
-						("password", self.password),
-						("type", self.type),
-						("output", self.output),
-						("host", self.host),
-						("player_api", self.player_api),
-						("enigma2_api", self.enigma2_api),
-						("xmltv_api", self.xmltv_api),
-						("full_url", self.full_url),
+						"playlist_info": dict([
+							("index", self.index),
+							("name", self.domain),
+							("protocol", self.protocol),
+							("domain", self.domain),
+							("port", self.port),
+							("username", self.username),
+							("password", self.password),
+							("type", self.type),
+							("output", self.output),
+							("host", self.host),
+							("player_api", self.player_api),
+							("enigma2_api", self.enigma2_api),
+							("xmltv_api", self.xmltv_api),
+							("full_url", self.full_url),
 						]),
 						"player_info": OrderedDict([
-						("livetype", self.livetype),
-						("vodtype", self.vodtype),
-						("catchuptype", self.catchuptype),
-						("epgshift", self.epgshift),
-						("livehidden", self.livehidden),
-						("vodhidden", self.vodhidden),
-						("serieshidden", self.serieshidden),
-						("showlive", self.showlive),
-						("showvod", self.showvod),
-						("showseries", self.showseries),
-						("showcatchup", self.showcatchup),
-						("epgtype", self.epgtype),
-						("epgquickshift", self.epgquickshift),
+							("livetype", self.livetype),
+							("vodtype", self.vodtype),
+							("catchuptype", self.catchuptype),
+							("epgshift", self.epgshift),
+							("livehidden", self.livehidden),
+							("vodhidden", self.vodhidden),
+							("serieshidden", self.serieshidden),
+							("showlive", self.showlive),
+							("showvod", self.showvod),
+							("showseries", self.showseries),
+							("showcatchup", self.showcatchup),
+							("epgtype", self.epgtype),
+							("epgquickshift", self.epgquickshift),
 						]),
-						
-						"data": dict([
-						("live_categories", []),
-						("vod_categories", []),
-						("series_categories", []),	
-		
-						("live_streams", []),
-						
-						("catchup", False),
-						("catchup_checked", False),
-					
-						("last_check", ''),
-						("epg_date", ''),
 
+						"data": dict([
+							("live_categories", []),
+							("vod_categories", []),
+							("series_categories", []),
+							("live_streams", []),
+							("catchup", False),
+							("catchup_checked", False),
+							("last_check", ''),
+							("epg_date", ''),
 						]),
 					})
 
 				self.index += 1
-			
+
 		self.removeOldPlaylists()
-		
-		
+
+
 	def removeOldPlaylists(self):
 		if self.playlists_all:
 			newList = []
@@ -306,11 +286,11 @@ class XStreamity_Main(Screen):
 						if str(playlist["playlist_info"]["domain"]) in line and 'username=' + str(playlist["playlist_info"]["username"]) in line and 'password=' + str(playlist["playlist_info"]["password"]) in line:
 							exists = True
 
-				if exists == True:
+				if exists:
 					newList.append(playlist)
 
 			self.playlists_all = newList
-				
+
 		self.getCurrentPlaying()
 
 
@@ -321,24 +301,24 @@ class XStreamity_Main(Screen):
 			glob.newPlayingServiceRef = self.session.nav.getCurrentlyPlayingServiceReference()
 			glob.newPlayingServiceRefString = glob.newPlayingServiceRef.toString()
 		self.delayedDownload()
-	
-		
-	#delay to allow splash screen to show
+
+
+	# delay to allow splash screen to show
 	def delayedDownload(self):
 		self.timer = eTimer()
-		try: 
+		try:
 			self.timer_conn = self.timer.timeout.connect(self.makeUrlList)
 		except:
 			try:
 				self.timer.callback.append(self.makeUrlList)
 			except:
-				self.self.makeUrlList()	
+				self.self.makeUrlList()
 		self.timer.start(5, True)
-		
-		
+
+
 	def makeUrlList(self):
 		self.url_list = []
-		x = 0 
+		x = 0
 		for playlists in self.playlists_all:
 			player_api = str(playlists["playlist_info"]["player_api"])
 			full_url = str(playlists["playlist_info"]["full_url"])
@@ -348,19 +328,19 @@ class XStreamity_Main(Screen):
 			if 'get.php' in full_url and domain != '' and username != '' and password != '':
 				self.url_list.append([player_api, x])
 			x += 1
-		
+
 		if self.url_list != []:
 			self.process_downloads()
-		
-				
+
+
 	def download_url(self, url):
 		index = url[1]
 		r = ''
-		#retries = Retry(total=1, status_forcelist=[408, 429, 500, 503, 504], method_whitelist=["HEAD", "GET", "OPTIONS"], backoff_factor = 3)
+		# retries = Retry(total=1, status_forcelist=[408, 429, 500, 503, 504], method_whitelist=["HEAD", "GET", "OPTIONS"], backoff_factor = 3)
 		adapter = HTTPAdapter(max_retries=0)
 		http = requests.Session()
 		http.mount("http://", adapter)
-		
+
 		try:
 			r = http.get(url[0], headers=hdr, stream=False, timeout=5, verify=False)
 			r.raise_for_status()
@@ -369,21 +349,21 @@ class XStreamity_Main(Screen):
 					response = r.json()
 					r.close()
 					return index, response
-					
+
 				except:
 					r.close()
 					return index, ''
-			
+
 		except requests.exceptions.ConnectionError as e:
 			print("Error Connecting: %s" % e)
 			r.close()
-			return index, ''	
-			
-		
-		except requests.exceptions.RequestException as e:  
+			return index, ''
+
+
+		except requests.exceptions.RequestException as e:
 			print(e)
 			r.close()
-			return index, ''	
+			return index, ''
 
 
 	def process_downloads(self):
@@ -421,8 +401,8 @@ class XStreamity_Main(Screen):
 		with open(json_file, 'wb') as f:
 			json.dump(self.playlists_all, f)
 		self.createSetup()
-		
-		
+
+
 	def createSetup(self):
 		self["splash"].hide()
 		self.list = []
@@ -439,19 +419,19 @@ class XStreamity_Main(Screen):
 			expires = ''
 			timeshift = 'EPG Timeshift: '
 
-			if playlist:	
+			if playlist:
 				if'name' in playlist['playlist_info']:
 					name = playlist['playlist_info']['name']
 				elif 'domain' in playlist['playlist_info']:
 					name = playlist['playlist_info']['domain']
-				
+
 				url = playlist['playlist_info']['host']
 
 				if 'user_info' in playlist and 'auth' in playlist['user_info']:
 						status = (_('Not Authorised'))
 
 						if playlist['user_info']['auth'] == 1:
-							
+
 							if playlist['user_info']['status'] == 'Active':
 								status = (_('Active'))
 							elif playlist['user_info']['status'] == 'Banned':
@@ -462,7 +442,7 @@ class XStreamity_Main(Screen):
 								status = (_('Expired'))
 
 							if status == (_('Active')):
-								
+
 								try:
 									expires = str("Expires: ") + str(datetime.fromtimestamp(int(playlist['user_info']['exp_date'])).strftime('%d-%m-%Y'))
 								except:
@@ -473,16 +453,16 @@ class XStreamity_Main(Screen):
 
 								maxc = str("Max Conn:")
 								maxnum = playlist['user_info']['max_connections']
-										
+
 				timeshift += str(playlist['player_info']['epgshift'])
 
 				self.list.append([index, name, url, expires, status, active, activenum, maxc, maxnum, timeshift])
 				index += 1
 
 		self.drawList = []
-		self.drawList = [self.buildListEntry(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8], x[9]) for x in self.list]
+		self.drawList = [self.buildListEntry(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9]) for x in self.list]
 		self["playlists"].setList(self.drawList)
-		
+
 		if self.list != []:
 			self['key_yellow'].setText(_('Edit'))
 			self['key_blue'].setText(_('Delete'))
@@ -509,7 +489,7 @@ class XStreamity_Main(Screen):
 			pixmap = LoadPixmap(cached=True, path=common_path + 'led_red.png')
 		if status == (_('Not Authorised')):
 			pixmap = LoadPixmap(cached=True, path=common_path + 'led_red.png')
-			
+
 		return(index, str(name), str(url), str(expires), str(status), pixmap, str(active), str(activenum), str(maxc), str(maxnum), str(timeshift))
 
 
@@ -539,7 +519,7 @@ class XStreamity_Main(Screen):
 		return
 
 
-	def deleteServer(self, answer = None):
+	def deleteServer(self, answer=None):
 		if self.list != []:
 			currentplaylist = glob.current_playlist.copy()
 
@@ -557,10 +537,9 @@ class XStreamity_Main(Screen):
 
 
 	def refresh(self):
-		
 		self["splash"].show()
 		self.playlists_all = []
-		
+
 		with open(json_file) as f:
 			try:
 				self.playlists_all = json.load(f)
@@ -581,7 +560,6 @@ class XStreamity_Main(Screen):
 
 			if self['playlists'].getIndex() < 5:
 				self["scroll_up"].hide()
-
 
 			if self['playlists'].getIndex()+1 > ((self['playlists'].count() // 5) * 5):
 				self["scroll_down"].hide()
@@ -612,8 +590,6 @@ class XStreamity_Main(Screen):
 		self.session.openWithCallback(self.settingsChanged, settings.XStreamity_Settings)
 
 
-	def settingsChanged(self, data = None):
-		if glob.changed == True:
+	def settingsChanged(self, data=None):
+		if glob.changed:
 			self.close()
-
-
