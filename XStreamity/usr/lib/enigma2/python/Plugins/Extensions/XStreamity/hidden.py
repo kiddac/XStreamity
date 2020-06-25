60daa67d80010051a67b822d8103b329c2582879
kiddac <kiddac2015@gmail.com>
2020-06-25 01:49:26 +0100

    Syntax Clean Up

--- XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/hidden.py
+++ XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/hidden.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# for localized messages     
+# for localized messages
 from . import _
 from collections import OrderedDict
 from Screens.Screen import Screen
@@ -25,17 +25,17 @@ class XStreamity_HiddenCategories(Screen):
 		skin = skin_path + 'hidden.xml'
 		self.category_type = category_type
 		self.channellist = channellist
-	
+
 		with open(skin, 'r') as f:
 			self.skin = f.read()
-			
+
 		self.setup_title = (_('Hidden Categories'))
 
 		self.startList = []
 		self.drawList = []
 		self['hidden_list'] = List(self.drawList, enableWrapAround=True)
 		self['hidden_list'].onSelectionChanged.append(self.getCurrentEntry)
-			
+
 		self.currentSelection = 0
 
 		self["key_red"] = StaticText(_('Cancel'))
@@ -61,32 +61,31 @@ class XStreamity_HiddenCategories(Screen):
 		self.onFirstExecBegin.append(self.loadHidden)
 		self.onLayoutFinish.append(self.__layoutFinished)
 
-		
-		
+
 	def __layoutFinished(self):
 		self.setTitle(self.setup_title)
 		self.getCurrentEntry()
-		
-		
+
+
 	def loadHidden(self):
 		self.playlists_all = []
 		self.hidelist = []
 		self.startList = []
-		
+
 		if self.category_type == "live":
 			self.hidelist = glob.current_playlist['player_info']['livehidden']
-			
+
 		elif self.category_type == "vod":
 			self.hidelist = glob.current_playlist['player_info']['vodhidden']
-				
+
 		elif self.category_type == "series":
 			self.hidelist = glob.current_playlist['player_info']['serieshidden']
-			
+
 		for item in self.channellist:
 			if item[4] not in self.hidelist:
 				self.startList.append([item[0], item[4], False])
 			elif item[4] in self.hidelist:
-				self.startList.append([item[0], item[4], True])	
+				self.startList.append([item[0], item[4], True])
 		self.refresh()
 
 
@@ -96,88 +95,88 @@ class XStreamity_HiddenCategories(Screen):
 		else:
 			pixmap = LoadPixmap(cached=True, path=common_path + "lock_off.png")
 		return(pixmap, str(name), str(category_id), enabled)
-		
-	  
+
+
 	def refresh(self):
 		self.drawList = []
 		self.drawList = [self.buildListEntry(x[0], x[1], x[2]) for x in self.startList]
 		self['hidden_list'].updateList(self.drawList)
-		
-		
+
+
 	def toggleSelection(self):
 		if len(self['hidden_list'].list) > 0:
 			idx = self['hidden_list'].getIndex()
 			self.startList[idx][2] = not self.startList[idx][2]
-			self.refresh()  
-			
-			
+			self.refresh()
+
+
 	def toggleAllSelection(self):
 		for idx, item in enumerate(self['hidden_list'].list):
 			self.startList[idx][2] = not self.startList[idx][2]
-		self.refresh()  
-		
-		
+		self.refresh()
+
+
 	def clearAllSelection(self):
 		for idx, item in enumerate(self['hidden_list'].list):
 			self.startList[idx][2] = False
-		self.refresh() 
-	
+		self.refresh()
+
 
 	def getCurrentEntry(self):
 		self.currentSelection = self['hidden_list'].getIndex()
-		
-		
+
+
 	def keyCancel(self):
 		self.close()
-		
-		
+
+
 	def keyGreen(self):
 		count = 0
 		for item in self.startList:
-			if item[2] == True:
+			if item[2] is True:
 				count += 1
-				
+
 		if count == len(self.channellist):
 			self.session.open(MessageBox, _("Error: All categories hidden. Please amend your selection."), MessageBox.TYPE_ERROR)
 			return
-			
-		
+
+
 		domain = glob.current_playlist['playlist_info']['domain']
 		username = glob.current_playlist['playlist_info']['username']
 		password = glob.current_playlist['playlist_info']['password']
-		
+
 		if self.category_type == "live":
 			glob.current_playlist['player_info']['livehidden'] = []
 
 			for item in self.startList:
-				if item[2] == True:
+				if item[2] is True:
 					glob.current_playlist['player_info']['livehidden'].append(item[1])
-			
+
 		elif self.category_type == "vod":
 			glob.current_playlist['player_info']['vodhidden'] = []
 			for item in self.startList:
-				if item[2] == True:
+				if item[2] is True:
 					glob.current_playlist['player_info']['vodhidden'].append(item[1])
-				
+
 		elif self.category_type == "series":
 			glob.current_playlist['player_info']['serieshidden'] = []
 			for item in self.startList:
-				if item[2] == True:
+				if item[2] is True:
 					glob.current_playlist['player_info']['serieshidden'].append(item[1])
-						
+
 		self.playlists_all = []
-		
+
 		with open(json_file) as f:
 			self.playlists_all = json.load(f, object_pairs_hook=OrderedDict)
-		 
+
 		x = 0
 		for playlist in self.playlists_all:
-			if playlist['playlist_info']['domain'] == str(domain).strip() and playlist['playlist_info']['username'] == str(username).strip() and playlist['playlist_info']['password'] == str(password).strip(): 
+			if playlist['playlist_info']['domain'] == str(domain).strip() and playlist['playlist_info']['username'] == str(username).strip() and playlist['playlist_info']['password'] == str(password).strip():
 				self.playlists_all[x] = glob.current_playlist
 				break
 			x += 1
-	
+
 		with open(json_file, 'w') as f:
 			json.dump(self.playlists_all, f)
-	
+
 		self.close()
