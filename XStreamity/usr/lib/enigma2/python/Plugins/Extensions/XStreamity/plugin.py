27615601a6f470051c0138e02993200d5e678b02
kiddac <kiddac2015@gmail.com>
2020-10-23 16:50:45 +0100

    v2.57

--- XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/plugin.py
+++ XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/plugin.py
@@ -11,7 +11,7 @@ from Components.config import config, ConfigSubsection, ConfigSelection, ConfigD
 import os
 import shutil
 
-VERSION = "2.56-20201019"
+VERSION = "2.57-20201023"
 screenwidth = getDesktop(0).size()
 
 dir_dst = "/etc/enigma2/xstreamity/"
