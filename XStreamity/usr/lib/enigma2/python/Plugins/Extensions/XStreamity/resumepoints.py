f7bbfb11db40af66912ce43c120e982c35d54072
kiddac <kiddac2015@gmail.com>
2024-04-08 22:22:42 +0100

    refactored resumepoints.py using chatgpt

--- XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/resumepoints.py
+++ XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/resumepoints.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 
 from time import time
 
 try:
     import cPickle
-except:
+except ImportError:
     import pickle as cPickle
 
 
@@ -14,26 +15,36 @@ def setResumePoint(session):
     global resumePointCache
     service = session.nav.getCurrentService()
 
-    ref = None
+    if not service:
+        return
+
+    ref = session.nav.getCurrentlyPlayingServiceReference()
+    if not ref or "http" not in ref.toString():
+        return
+
+    seek = service.seek()
+    if not seek:
+        return
+
+    pos = seek.getPlayPosition()
+    if pos[0]:  # If seeking is not possible, return
+        return
+
     if session.nav.getCurrentlyPlayingServiceReference():
         if "http" in session.nav.getCurrentlyPlayingServiceReference().toString():
             ref = session.nav.getCurrentlyPlayingServiceReference()
 
-    if (service is not None) and (ref is not None):
-        seek = service.seek()
-        if seek:
-            pos = seek.getPlayPosition()
-            if not pos[0]:
-                key = ref.toString()
-                lru = int(time())
-                length = seek.getLength()
-                if length:
-                    length = length[1]
-                else:
-                    length = None
-                resumePointCache[key] = [lru, pos[1], length]
+    key = ref.toString()
+    lru = int(time())
+    length = seek.getLength()
+    if length:
+        length = length[1]
+    else:
+        length = None
 
-                saveResumePoints()
+    resumePointCache[key] = [lru, pos[1], length]
+
+    saveResumePoints()
 
 
 def delResumePoint(ref):
@@ -48,38 +59,36 @@ def delResumePoint(ref):
 
 def getResumePoint(session):
     global resumePointCache
+
     resumePointCache = loadResumePoints()
 
-    ref = None
-    if session.nav.getCurrentlyPlayingServiceReference():
-        if "http" in session.nav.getCurrentlyPlayingServiceReference().toString():
-            ref = session.nav.getCurrentlyPlayingServiceReference()
-
-    if (ref is not None) and (ref.type != 1):
+    ref = session.nav.getCurrentlyPlayingServiceReference()
+    if ref and "http" in ref.toString() and ref.type != 1:
         try:
-            entry = resumePointCache[ref.toString()]
-            entry[0] = int(time())  # update LRU timestamp
-            return entry[1]
+            entry = resumePointCache.get(ref.toString())
+            if entry:
+                entry[0] = int(time())  # Update LRU timestamp
+                return entry[1]
         except KeyError:
-            return None
+            pass
+
+    return None
 
 
 def saveResumePoints():
     global resumePointCache
     try:
-        f = open("/etc/enigma2/xstreamity/resumepoints.pkl", "wb")
-        cPickle.dump(resumePointCache, f, cPickle.HIGHEST_PROTOCOL)
-        f.close()
+        with open(os.path.join("/etc/enigma2/xstreamity", "resumepoints.pkl"), "wb") as f:
+            cPickle.dump(resumePointCache, f, cPickle.HIGHEST_PROTOCOL)
     except Exception as e:
         print("[XStreamity] Failed to write resumepoints:", e)
 
 
 def loadResumePoints():
     try:
-        f = open("/etc/enigma2/xstreamity/resumepoints.pkl", "rb")
-        PickleFile = cPickle.load(f)
-        f.close()
-        return PickleFile
+        with open(os.path.join("/etc/enigma2/xstreamity", "resumepoints.pkl"), "rb") as f:
+            PickleFile = cPickle.load(f)
+            return PickleFile
     except Exception as e:
         print("[XStreamity] Failed to load resumepoints:", e)
         return {}
