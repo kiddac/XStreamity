9fffd6838dd7fcaa4cc76f23932776b2f5f986ba
kiddac <kiddac2015@gmail.com>
2024-05-09 20:19:12 +0100

    v4.45 - user-agent update

--- XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/plugin.py
+++ XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/plugin.py
@@ -184,9 +184,9 @@ else:
 
 font_folder = os.path.join(dir_plugins, "fonts/")
 
-hdr = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-}
+# hdr = {"User-Agent": "Enigma2 - XStreamity Plugin"}
+# hdr = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
+hdr = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
 
 # create folder for working files
 if not os.path.exists(dir_etc):
